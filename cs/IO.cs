0c0cd423dc8e655abdaed63bba890aa79cdc316d
Jordan Gibson <41699463+jordanmgibson@users.noreply.github.com>
2022-08-29 09:27:28 -0400

    IO and data structures exercise code

--- cs/IO.cs
+++ cs/IO.cs
@@ -1,3 +1,6 @@
+// There was a substatial time difference in the computing times each method took. The Hash List was the fastest, the List was in the middle for computing time, and the sorted set was the longest computing time.
+// For looking up words in the dictionary, the normal List was by far the fastest. The sorted seta and hash set took similar amounts of time but they both took more than 8 minutes longer than the List.
+// There are 972 three letter words.
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,6 +25,45 @@ namespace cs {
                     output.WriteLine();
                 }
             }
+        }
+        // This method is complete and correct.
+        public static List<string> ReadWordsList(string fileName) {
+            List<string> result = new List<string>();
+            using(StreamReader input = new StreamReader(fileName + ".txt"))
+            {
+                while (!input.EndOfStream)
+                {
+                    string line = input.ReadLine();
+                    result.Add(line);
+                }
+            }
+            return result;
+        }
+        // This method is complete and correct.
+        public static SortedSet<string> ReadWordsSortedSet(string fileName) {
+            SortedSet<string> result = new SortedSet<string>();
+            using (StreamReader input = new StreamReader(fileName + ".txt"))
+            {
+                while (!input.EndOfStream)
+                {
+                    string line = input.ReadLine();
+                    result.Add(line);
+                }
+            }
+            return result;
+        }
+        // This method is complete and correct.
+        public static HashSet<string> ReadWordsHashSet(string fileName) {
+            HashSet<string> result = new HashSet<string>();
+            using (StreamReader input = new StreamReader(fileName + ".txt"))
+            {
+                while (!input.EndOfStream)
+                {
+                    string line = input.ReadLine();
+                    result.Add(line);
+                }
+            }
+            return result;
         }
     }
 }
--- cs/Main.cs
+++ cs/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace cs {
@@ -25,6 +26,81 @@ namespace cs {
             //myquiz.Delegates.QuizMain(args);
             //Events.EventsMain(args);
             //Quizzes.QuizzesMain(args);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            /*foreach (var word in IO.ReadWordsList("words")) {
+                Console.Write("{0}\t", word);
+            }
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);*/
+            /*Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (var word in IO.ReadWordsSortedSet("words")) {
+                Console.Write("{0}\t", word);
+            }
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);*/
+            /*Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (var word in IO.ReadWordsHashSet("words")) {
+                Console.Write("{0}\t", word);
+            }
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);*/
+            /*string[] words = { "COMPUTERS", "ZYMOTIC", "AARDVARK", "WORD", "DATABASIC" };
+            int count = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (var word in words)
+                {
+                    if (IO.ReadWordsList("words").Contains(word))
+                    {
+                        count++;
+                    }
+                }
+            }
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);
+            Console.WriteLine(count);
+            count = 0;
+            stopWatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (var word in words)
+                {
+                    if (IO.ReadWordsSortedSet("words").Contains(word))
+                    {
+                        count++;
+                    }
+                }
+            }
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);
+            Console.WriteLine(count);
+            count = 0;
+            stopWatch.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                foreach (var word in words)
+                {
+                    if (IO.ReadWordsHashSet("words").Contains(word))
+                    {
+                        count++;
+                    }
+                }
+            }
+            stopWatch.Stop();
+            Console.WriteLine(stopWatch.Elapsed);
+            Console.WriteLine(count);*/
+            int threeLetter = 0;
+            foreach (var word in IO.ReadWordsList("words"))
+            {
+                if (word.Length == 3)
+                {
+                    threeLetter++;
+                }
+            }
+            Console.WriteLine(threeLetter);
         }
     }
 }
